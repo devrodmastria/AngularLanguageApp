b62761802038ad955264b94b0d217a3ab3fd7958
Rod Mastria <rodmastria@gmail.com>
2024-04-10 18:10:38 -0400

    build 16.27

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 //const googleLoginProvider = import.meta.resolve("secretgooglecode", "NODE_ENV").valueOf()
 // const googleLoginProvider = import.meta.resolve("secretgooglecode").valueOf();
 // const googleLoginProvider = process.env['GOOGLE_LOGIN_MODULE'] || "googleKey";
-const googleLoginProvider = process.env['${secretgooglecode}'] || "NO LOGIN YET"
+const googleLoginProvider = '${secretgooglecode}'
 
 export const appConfig: ApplicationConfig = {
   providers: [
