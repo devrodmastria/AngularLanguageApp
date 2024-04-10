aa37af03fc5ab84df962fba9f787438c9c6f9934
Rod Mastria <rodmastria@gmail.com>
2024-04-10 13:46:04 -0400

    build 16.15

--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -8,10 +8,14 @@ import { Observable } from 'rxjs';
 import { DictionaryModel } from '../Models/dictionary-model';
 // import { secretkey } from '../secret';
 import { FavoriteWord } from '../Models/favorite-words';
+import dotenv from 'dotenv'
+import { toNamespacedPath } from 'path';
+dotenv.config({path:toNamespacedPath("secretkey")})
 
 //const secretkey = import.meta.resolve("secretkey", "NODE_ENV").valueOf()
-const secretkey = process.env.secretkey; //DICTIONARY_API
 
+const secretkey = process.env["secretkey"]?.toString; //DICTIONARY_API
+console.log(secretkey)
 
 @Injectable({
   providedIn: 'root'
